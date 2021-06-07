5bdd1161865e718d026b4a614521b30ed35d5776
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-07 13:25:30 +0800

    add test

--- test.py
+++ test.py
@@ -7,6 +7,15 @@ class TestCase(unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(example.addition(1, 2), 3)
 
+    def test_sub_1(self):
+        self.assertEqual(example.subtraction(3, 1), 2)
+
+    def test_mul_1(self):
+        self.assertEqual(example.multiplication(2, 2), 4)
+
+    def test_div_1(self):
+        self.assertEqual(example.division(6, 2), 3)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
