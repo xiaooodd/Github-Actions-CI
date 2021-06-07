112cf7244d13a87a36f57b7b730ef0b9948f2e78
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-07 13:18:16 +0800

    fix error

--- example.py
+++ example.py
@@ -1,3 +1,2 @@
 def addition(a, b):
     return a + b
-    
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import unittest
 import example
 
+
 class TestCase(unittest.TestCase):
+
     def test_add_1(self):
-        self.assertEqual(example.addition(1,2), 3)
+        self.assertEqual(example.addition(1, 2), 3)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
