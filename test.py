7333d5bdab54ea9cd0b5b54602968a4ef6deb79e
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-07 13:07:49 +0800

    add tests

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+import example
+
+class TestCase(unittest.TestCase):
+    def test_add_1(self):
+        self.assertEqual(example.addition(1,2), 3)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
